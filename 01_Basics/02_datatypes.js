724dad7f8c2140916275e471ef1532481644d7b5
Divya-tech469 <divy123shinde@gmail.com>
2025-11-22 02:04:09 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) // we are using nodejs not browser
+
+console.log(3
+    +
+    3) // This is not good
+    // code readability that should be high 
+
+console.log("Divya")
+let name="Divya"
+let age=28
+let isLoggedIn=false
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => unique
+
+//object 
+
+console.log(typeof age);   //type number
+console.log(typeof null);   //type object
+console.log(typeof undefined);  // type undefined
