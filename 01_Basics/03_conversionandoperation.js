fc4610238b53dd677567d0bd9a273887b137e81c
Divya-tech469 <divy123shinde@gmail.com>
2025-12-12 13:01:11 +0000

    new changes

--- 01_Basics/03_conversionandoperation.js
+++ 01_Basics/03_conversionandoperation.js
@@ -1,15 +1,15 @@
 let score=33
 let score1="33abc"
-console.log(typeof score)
-console.log(typeof(score1))
+// console.log(typeof score)
+// console.log(typeof(score1))
 
 let valueNumber=Number(score1)
-console.log(typeof valueNumber)
-console.log(valueNumber)
+// console.log(typeof valueNumber)
+// console.log(valueNumber)
 
 let score2=null
 let valueNumber1=Number(score2)
-console.log(valueNumber1)
+//console.log(valueNumber1)
 
 //"33" convert 33
 //"33abc" convert NaN
@@ -18,11 +18,41 @@ console.log(valueNumber1)
 
 let isloggedIn=1
 let booleanIsloggedIn=Boolean(isloggedIn)
-console.log(booleanIsloggedIn);
+//console.log(booleanIsloggedIn);
 // 1=> true; 0=>false
 //"" => false; "divya" => true
 
 let someNumber=33
 let StringNumber=String(someNumber)
-console.log(StringNumber)
-console.log(typeof StringNumber)
+//console.log(StringNumber)
+//console.log(typeof StringNumber)
+
+//***********************************Operations*************************************** */
+
+let value=3
+let negvalue=-value
+//console.log(negvalue)
+
+let Str1="hello"
+let Str2="Divya"
+let str3=Str1+Str2
+//console.log(str3)
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+// console.log(true)
+// console.log(+true)
+// console.log(+"")
+let gameCounter=100
+gameCounter++
+console.log(gameCounter)
+
+
+
+
+
+
