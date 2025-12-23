6fb68b49a64c9c79847115be4c804639c0898106
Divya-tech469 <divy123shinde@gmail.com>
2025-12-23 09:29:04 +0000

    datatypes

---
+++ 01_Basics/datatypes_summary.js
@@ -0,0 +1,18 @@
+// primitive
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+//Reference(Non primitive)
+//Array, Objects, Functions
+
+const score = 100
+const scoreValue=100.3
+
+const isLoggedIn=false
+const outsideTemp=null
+let userEmail; // value is undefined
+
+const id=Symbol('123')
+const anotherId=symbol('123')
+console.log(id === anotherId);
+
+
